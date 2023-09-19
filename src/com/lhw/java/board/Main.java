d075f37795127c7f4e8ea6731ee1c9a1eaad6b7c
LHW112 <jk129k1211@gmail.com>
2023-09-19 13:00:08 +0900

    시작알림 메세지 출력

--- src/com/lhw/java/board/Main.java
+++ src/com/lhw/java/board/Main.java
@@ -4,6 +4,9 @@ package com.lhw.java.board;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("== 자바 텍스트 게시판==");
+    System.out.println("==프로그램 시작==");
+    System.out.println("명령)");
+    System.out.println("==프로그램 종료 ==");
         }
     }
