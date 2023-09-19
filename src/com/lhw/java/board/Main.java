e00f22521ffd10fc3bf74740f4b10addf64a41c9
LHW112 <jk129k1211@gmail.com>
2023-09-19 13:33:31 +0900

    게시물 등록 시마다 게시물 번호가 증가 되도록

--- src/com/lhw/java/board/Main.java
+++ src/com/lhw/java/board/Main.java
@@ -10,16 +10,32 @@ public class Main {
         System.out.println("== 자바 텍스트 게시판==");
         System.out.println("==프로그램 시작==");
 
+        int articleLastid = 0;
+
         while (true){
             System.out.printf("명령)");
             String cmd = sc.nextLine();
 
-            if (cmd.equals("exit")){
+            if (cmd.equals("/usr/article/write")){
+                System.out.println("==게시물 등록==");
+                System.out.printf("제목: ");
+                String title = sc.nextLine();
+                System.out.printf("내용: ");
+                String content = sc.nextLine();
+
+                int id = articleLastid + 1;
+                articleLastid++;
+                System.out.printf("%d번 게시물이 등록 되었습니다.\n", id);
+
+            }
+
+
+            else if (cmd.equals("exit")){
                 System.out.println("프로그램을 종료 합니다.");
                 break;
             }
-
-            System.out.printf("입력 된 명령어 : %s\n",cmd);
+            else
+                System.out.println("잘못된 명령어 입니다.");
         }
 
 
